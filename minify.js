f7278279cc9aaff1c43d20183c4f9bd88a37d335
Damien Erambert <damien.erambert@me.com>
2014-05-19 00:26:27 +0200

    [Metæ] Updated minify.js for removing the emoji-pngs source

--- minify.js
+++ minify.js
@@ -1,7 +1,22 @@
 var minifier = require("minifier");
+var rimraf = require("rimraf");
 var options = {
 	template : "{{filename}}.{{ext}}"
 }
+
+
+var filesToDelete = [
+	"source/emoji-pngs/config.rb",
+	"source/emoji-pngs/emojis",
+	"source/emoji-pngs/emojis2x",
+	"source/emoji-pngs/sass",
+];
+
+filesToDelete.forEach(function(file) {
+	rimraf(file, function(err) {
+		if (err) throw err;
+	});
+});
 
 minifier.on('error', function(err) {
 	console.error(err);
