abf3fe674fa1d3339381cc64a7886f9187e5ff4d
Damien Erambert <damien.erambert@me.com>
2014-07-30 20:16:43 +0200

    Now using gulp-include

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,13 @@ var gulp = require("gulp");
 var minifyCSS = require("gulp-minify-css");
 var uglify = require("gulp-uglify");
 var concat = require("gulp-concat");
+var include = require("gulp-include");
 
 var jsFiles = ["source/js/content.js"];
 
 gulp.task("concat", function() {
 	gulp.src(jsFiles)
+		.pipe(include())
 		.pipe(concat('BTD.js'))
 		.pipe(gulp.dest('source/js/'))
 })
