d02dee85503f59f3486811d4114f410377ac5d01
Damien Erambert <damien.erambert@me.com>
2014-07-31 15:52:44 +0200

    Added Sass w/ gulp-sass (more than enough)

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var minifyCSS = require("gulp-minify-css");
 var uglify = require("gulp-uglify");
 var concat = require("gulp-concat");
 var include = require("gulp-include");
+var sass = require('gulp-sass');
 
 var jsFiles = ["source/js/content.js"];
 
@@ -10,9 +11,16 @@ gulp.task("concat", function() {
 	gulp.src(jsFiles)
 		.pipe(include())
 		.pipe(concat('BTD.js'))
-		.pipe(gulp.dest('source/js/'))
+		.pipe(gulp.dest('source/js/'));
+});
+
+gulp.task('sass', function() {
+	gulp.src('source/styles/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('source/styles'));
 })
 
 gulp.task("watch", function() {
 	gulp.watch('source/js/**/*.js', ['concat']);
-})
+	gulp.watch('source/styles/*.scss', ['sass']);
+});
