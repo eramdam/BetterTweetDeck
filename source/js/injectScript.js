79409247bd31e42a4fed08f34ea4df92ce9e095f
Damien Erambert <damien.erambert@me.com>
2014-07-31 23:38:15 +0200

    Added Handlebars and injectScript

---
+++ source/js/injectScript.js
@@ -0,0 +1,105 @@
+//////////////////////////////////////////////////////////////////////////////////////////////
+// Copyright(C) 2010 Abdullah Ali, voodooattack@hotmail.com                                 //
+//////////////////////////////////////////////////////////////////////////////////////////////
+// Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php       //
+//////////////////////////////////////////////////////////////////////////////////////////////
+ 
+// Injects a script into the DOM, the new script gets executed in the original page's
+// context instead of the active content-script context.
+//
+//    Parameters:
+//            source: [string/function]
+//            (2..n): Function arguments if a function was passed as the first parameter.
+ 
+ 
+function injectScript(source)
+{
+     
+    // Utilities
+    var isFunction = function (arg) { 
+        return (Object.prototype.toString.call(arg) == "[object Function]"); 
+    };
+     
+    var jsEscape = function (str) { 
+        // Replaces quotes with numerical escape sequences to
+        // avoid single-quote-double-quote-hell, also helps by escaping HTML special chars.
+        if (!str || !str.length) return str;
+        // use \W in the square brackets if you have trouble with any values.
+        var r = /['"<>\/]/g, result = "", l = 0, c; 
+        do{    c = r.exec(str);
+            result += (c ? (str.substring(l, r.lastIndex-1) + "\\x" + 
+                c[0].charCodeAt(0).toString(16)) : (str.substring(l)));
+        } while (c && ((l = r.lastIndex) > 0))
+        return (result.length ? result : str);
+    };
+ 
+    var bFunction = isFunction(source);
+    var elem = document.createElement("script");    // create the new script element.
+    var script, ret, id = "";
+ 
+    if (bFunction)
+    {
+        // We're dealing with a function, prepare the arguments.
+        var args = [];
+ 
+        for (var i = 1; i < arguments.length; i++)
+        {
+            var raw = arguments[i];
+            var arg;
+ 
+            if (isFunction(raw))    // argument is a function.
+                arg = "eval(\"" + jsEscape("(" + raw.toString() + ")") + "\")";
+            else if (Object.prototype.toString.call(raw) == '[object Date]') // Date
+                arg = "(new Date(" + raw.getTime().toString() + "))";
+            else if (Object.prototype.toString.call(raw) == '[object RegExp]') // RegExp
+                arg = "(new RegExp(" + raw.toString() + "))";
+            else if (typeof raw === 'string' || typeof raw === 'object') // String or another object
+                arg = "JSON.parse(\"" + jsEscape(JSON.stringify(raw)) + "\")";
+            else
+                arg = raw.toString(); // Anything else number/boolean
+ 
+            args.push(arg);    // push the new argument on the list
+        }
+ 
+        // generate a random id string for the script block
+        while (id.length < 16) id += String.fromCharCode(((!id.length || Math.random() > 0.5) ?
+            0x61 + Math.floor(Math.random() * 0x19) : 0x30 + Math.floor(Math.random() * 0x9 )));
+ 
+        // build the final script string, wrapping the original in a boot-strapper/proxy:
+        script = "(function(){var value={callResult: null, throwValue: false};try{value.callResult=(("+
+            source.toString()+")("+args.join()+"));}catch(e){value.throwValue=true;value.callResult=e;};"+
+            "document.getElementById('"+id+"').innerText=JSON.stringify(value);})();";
+ 
+        elem.id = id;
+    }
+    else // plain string, just copy it over.
+    {
+        script = source;
+    }
+ 
+    elem.type = "text/javascript";
+    elem.innerHTML = script;
+ 
+    // insert the element into the DOM (it starts to execute instantly)
+    document.head.appendChild(elem);
+ 
+    if (bFunction)
+    {
+        // get the return value from our function:
+        ret = JSON.parse(elem.innerText);
+ 
+        // remove the now-useless clutter.
+        elem.parentNode.removeChild(elem);
+ 
+        // make sure the garbage collector picks it instantly. (and hope it does)
+        delete (elem);
+ 
+        // see if our returned value was thrown or not
+        if (ret.throwValue)
+            throw (ret.callResult);
+        else
+            return (ret.callResult);
+    }
+    else // plain text insertion, return the new script element.
+        return (elem);
+}
