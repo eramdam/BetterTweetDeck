9a427dca2283d967f6af40d41bc704585bbbbd8e
Damien Erambert <damien.erambert@me.com>
2014-08-01 04:06:22 +0200

    Fixed the jake build command

--- jakefile.js
+++ jakefile.js
@@ -2,7 +2,7 @@ desc('Build the extension .zip file')
 task('build', function() {
 	jake.exec('gulp uglify');
 	jake.exec('gulp sass');
-	jake.exec('cp -R source build; rm build/**/*.scss; rm build/js/*.js; cp source/js/{BTD,libs}.js build/js/');
+	jake.exec('rm -r build/; cp -R source/ build/; rm build/**/*.scss; rm build/js/*.js; cp source/js/{BTD,libs}.js build/js/; zip -r build.zip build/');
 })
 
 
