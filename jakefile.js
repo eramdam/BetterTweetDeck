49450feba7b3966785a7dfb1c7bf48432343afcf
Damien Erambert <damien.erambert@me.com>
2014-08-01 20:46:29 +0200

    Better use of jake.exec in jake build

--- jakefile.js
+++ jakefile.js
@@ -1,8 +1,17 @@
 desc('Build the extension .zip file')
 task('build', function() {
-	jake.exec('gulp uglify');
-	jake.exec('gulp sass');
-	jake.exec('rm -r build/; cp -R source/ build/; rm build/**/*.scss; rm build/js/*.js; cp source/js/{BTD,libs}.js build/js/; zip -r build.zip build/');
+	var cmds = [
+		"gulp uglify",
+		"gulp sass",
+		"rm -r build*",
+		"cp -R source/ build/",
+		"rm build/**/*.scss",
+		"rm build/js/*.js",
+		"cp source/js/{BTD,libs}.js build/js/",
+		"zip -r build.zip build/"
+	];
+
+	jake.exec(cmds, {printStdout: true, printStderr: true, breakOnError: false});
 })
 
 
