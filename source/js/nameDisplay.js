4dff49433d56347a4c896356810a8924f300fd6f
Damien Erambert <damien.erambert@me.com>
2014-08-02 12:20:08 +0200

    Fixed nameDisplay for direct messages

--- source/js/nameDisplay.js
+++ source/js/nameDisplay.js
@@ -2,14 +2,14 @@ function nameDisplay (el) {
 	if (el.querySelector('.tweet-detail')) {
 		setTimeout(function() {
 			var username = el.querySelector('.account-summary a[rel=user]').href.split('/').pop();
-			var fullname = el.querySelector('.account-summary b.fullname').innerText;
+			var fullname = el.querySelector('.account-summary b.fullname').innerHTML;
 
 			el.querySelector('span.username').innerText = fullname;
 			el.querySelector('b.fullname').innerText = username;
 		}, 0);
 
-	} else if (el.querySelector('.fullname') && el.querySelector('.username')) {
-		var fullname = el.querySelector('.fullname').innerText;
+	} else if ( (el.querySelector('.fullname') && el.querySelector('.username')) && el.querySelectorAll('ul.tweet-actions > *').length != 1) {
+		var fullname = el.querySelector('.fullname').innerHTML;
 		// @TODO This produces an error when profil modals are opened and sometimes with tweets/RT
 		var username = el.querySelector('header a[rel=user]').href.split('/').pop();
 		el.querySelector('.username').innerText = fullname;
@@ -27,5 +27,10 @@ function nameDisplay (el) {
 	} else if (el.querySelector('.nbfc a[rel=user]') && (settings.name_display == "inverted" || settings.name_display == "username")) {
 		var username = el.querySelector('a[rel=user]').href.split('/').pop();
 		el.querySelector('.nbfc a[rel=user]').innerText = username;
+	} else if (el.querySelectorAll('ul.tweet-actions > *').length == 1) {
+		var username = el.querySelector('a[rel=user]').href.split('/').pop();
+		var fullname =  el.querySelector('b.fullname').innerHTML;
+		el.querySelector('span.username').innerText = fullname;
+		el.querySelector('b.fullname').innerHTML = username;
 	}
 }
