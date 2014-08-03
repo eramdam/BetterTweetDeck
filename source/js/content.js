0a1120bf7c8cf978773242e439374dc2ff238a86
Damien Erambert <damien.erambert@me.com>
2014-08-03 12:43:13 +0200

    Moved SettingsButton()

--- source/js/content.js
+++ source/js/content.js
@@ -1,5 +1,63 @@
 "use strict";
 
+// Production steps of ECMA-262, Edition 5, 15.4.4.18
+// Reference: http://es5.github.com/#x15.4.4.18
+
+Array.prototype.forEach = function(callback, thisArg) {
+    var T, k;
+
+    if (this == null) {
+        throw new TypeError(" this is null or not defined");
+    }
+
+    // 1. Let O be the result of calling ToObject passing the |this| value as the argument.
+    var O = Object(this);
+
+    // 2. Let lenValue be the result of calling the Get internal method of O with the argument "length".
+    // 3. Let len be ToUint32(lenValue).
+    var len = O.length >>> 0;
+
+    // 4. If IsCallable(callback) is false, throw a TypeError exception.
+    // See: http://es5.github.com/#x9.11
+    if (typeof callback !== "function") {
+        throw new TypeError(callback + " is not a function");
+    }
+
+    // 5. If thisArg was supplied, let T be thisArg; else let T be undefined.
+    if (arguments.length > 1) {
+        T = thisArg;
+    }
+
+    // 6. Let k be 0
+    k = 0;
+
+    // 7. Repeat, while k < len
+    while (k < len) {
+
+        var kValue;
+
+        // a. Let Pk be ToString(k).
+        //   This is implicit for LHS operands of the in operator
+        // b. Let kPresent be the result of calling the HasProperty internal method of O with argument Pk.
+        //   This step can be combined with c
+        // c. If kPresent is true, then
+        if (k in O) {
+
+            // i. Let kValue be the result of calling the Get internal method of O with argument Pk.
+            kValue = O[k];
+
+            // ii. Call the Call internal method of callback with T as the this value and
+            // argument list containing kValue, k, and O.
+            callback.call(T, kValue, k, O);
+        }
+        // d. Increase k by 1.
+        k++;
+    }
+    // 8. return undefined
+};
+
+NodeList.prototype.forEach = Array.prototype.forEach; 
+HTMLCollection.prototype.forEach = Array.prototype.forEach; // Because of https://bugzilla.mozilla.org/show_bug.cgi?id=14869
 
 (function() {
 	var settings;
@@ -23,12 +81,7 @@
 
 				addEmojiPanel();
 				injectScript(mediaPreviewSize);
-				var settingsPage = chrome.extension.getURL("options/options.html");
-				var settingsButton = '<a class="btd-settings js-header-action link-clean cf app-nav-link padding-hl" data-title="BTD Settings" data-action href="'+settingsPage+'"> <div class="obj-left"> <i class="icon icon-sliders icon-large icon-unread-color"></i> </div> <div class="nbfc padding-ts hide-condensed">BTD Settings</div> </a>';
-				document.querySelector(".js-header-action.link-clean.app-nav-link[data-action=change-sidebar-width]").insertAdjacentHTML("beforebegin", settingsButton);
-				document.querySelector(".btd-settings").addEventListener("click", function() {
-					window.open(settingsPage);
-				});
+				SettingsButton();
 			}
 		}
 	});
@@ -218,5 +271,14 @@
 		// CloseOpenModal(null, true);
 		konamiTweets();
 	});
+
+	function SettingsButton() {
+		var settingsPage = chrome.extension.getURL("options/options.html");
+		var settingsButton = '<a class="btd-settings js-header-action link-clean cf app-nav-link padding-hl" data-title="BTD Settings" data-action href="' + settingsPage + '"> <div class="obj-left"> <i class="icon icon-sliders icon-large icon-unread-color"></i> </div> <div class="nbfc padding-ts hide-condensed">BTD Settings</div> </a>';
+		document.querySelector(".js-header-action.link-clean.app-nav-link[data-action=change-sidebar-width]").insertAdjacentHTML("beforebegin", settingsButton);
+		document.querySelector(".btd-settings").addEventListener("click", function() {
+			window.open(settingsPage);
+		});
+	}
 
 })();
