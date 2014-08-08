eramdam/BetterTweetDeck

commit b078e0ba24578b17576ae051d789b3baeea6e183

Avoid error spawning in Thumbnails.js

source/js/Thumbnails.js
@@ -21,7 +21,10 @@ function AddPreview(target, finalURL, content, isHTML, size) {
 	} else if (size == "large") {
 		var tpl = Handlebars.compile(templates.media_large);
 	}
-	var html = tpl(dataThumbs);
+	
+	if (tpl) {
+		var html = tpl(dataThumbs);
+	}
 
 	if (size == "large") {
 		target.querySelector('.js-tweet').insertAdjacentHTML("afterend", html);
@@ -40,8 +43,10 @@ function createMediaModal(sourceTweet, contentURL, content, isHTML) {
 
 	if (sourceTweet.querySelector('[data-key]')) {
 		var tweetKey = sourceTweet.querySelector('[data-key]').getAttribute('data-key');
-	} else {
+	} else if (sourceTweet.parentNode.querySelector('[data-key]')) {
 		var tweetKey = sourceTweet.parentNode.querySelector('[data-key]').getAttribute('data-key')
+	} else {
+		return;
 	}
 
 	if (!isHTML) {

commit 4bd0574df08ba53044575c8453c96bff751bd774

Fixed bug introduced in b078e0b

source/js/Thumbnails.js
@@ -24,6 +24,8 @@ function AddPreview(target, finalURL, content, isHTML, size) {
 	
 	if (tpl) {
 		var html = tpl(dataThumbs);
+	} else {
+		return;
 	}
 
 	if (size == "large") {