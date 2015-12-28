1140e35aaea7515156f4411b4a79e0756532ef37
Damien Erambert <damien.erambert@me.com>
2015-12-27 19:36:12 -0800

    :cookie: "full_after_24h" option functional

--- src/js/background.js
+++ src/js/background.js
@@ -4,16 +4,16 @@ import * as logger from './util/logger';
 BHelper.settings.setAll({
   'installed_date': 42,
   'ts': 'absolute_metric',
-  'full_aft_24': false,
+  'full_aft_24': true,
   'nm_disp': 'both',
-  'no_hearts': true,
+  'no_hearts': false,
   'no_tco': true,
   'flash_tweets': 'mentions',
   'css': {
     'round_pic': true,
     'no_col_icns': true,
     'no_play_btn': true,
-    'gray_icns_notifs': true,
+    'gray_icns_notifs': false,
     'minimal_mode': true,
     'small_icns_compose': true,
     'usrname_only_typeahed': true
@@ -23,5 +23,5 @@ BHelper.settings.setAll({
     'short_txt': false
   }
 }, (settings) => {
-  logger.debug(`Default settings to`, settings);
+  console.debug(`Default settings to`, settings);
 }, true);
--- src/js/util/timestamp.js
+++ src/js/util/timestamp.js
@@ -2,14 +2,36 @@ import fecha from 'fecha';
 import * as BHelper from './browserHelper';
 
 let timestampMode;
+let fullAfter24;
 
 BHelper.settings.get('ts', (val) => {
   timestampMode = val;
 });
 
+BHelper.settings.get('full_aft_24', (val) => {
+  fullAfter24 = val;
+});
+
 const formatMaps = {
-  'absolute_us': 'MM/DD/YY hh:mm a',
-  'absolute_metric': 'DD/MM/YY HH:mm a'
+  'absolute_us': {
+    'full': 'MM/DD/YY hh:mm a',
+    'short': 'hh:mm a'
+  },
+  'absolute_metric': {
+    'full': 'DD/MM/YY HH:mm',
+    'short': 'HH:mm'
+  }
+};
+
+const moreThan24 = (d) => {
+  return new Date().getTime() - d.getTime() <= 60 * 60 * 24000;
+};
+
+const getFormat = (dateObject, timestampMode) => {
+  if (fullAfter24 && moreThan24(dateObject))
+    return formatMaps[timestampMode].short;
+
+  return formatMaps[timestampMode].full;
 };
 
 const getDateObject = (dateString) => {
@@ -23,5 +45,7 @@ export default function timestampOnElement(element, dateString) {
   if (timestampMode === 'relative')
     return;
 
-  element.innerHTML = fecha.format(getDateObject(dateString), formatMaps[timestampMode]);
+  const d = getDateObject(dateString);
+
+  element.innerHTML = fecha.format(d, getFormat(d, timestampMode));
 }
