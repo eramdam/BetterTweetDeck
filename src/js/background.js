ea6a2c0cee821aef26fc38d906d979e9a08e7ea2
Damien Erambert <damien.erambert@me.com>
2016-05-09 23:54:17 -0700

    update CSS of dark theme in minimal mode

--- src/js/background.js
+++ src/js/background.js
@@ -13,7 +13,7 @@ BHelper.settings.setAll({
     round_pic: true,
     no_col_icns: true,
     no_play_btn: true,
-    gray_icns_notifs: false,
+    gray_icns_notifs: true,
     minimal_mode: true,
     small_icns_compose: true,
     usrname_only_typeahed: true,
