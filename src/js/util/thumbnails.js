0cee63c8be5e62c6a5b10f5f6824c60176b2f9b8
Damien Erambert <damien.erambert@me.com>
2016-08-08 12:10:39 -0700

    instagram without embed.ly

--- src/js/util/thumbnails.js
+++ src/js/util/thumbnails.js
@@ -304,6 +304,29 @@ const schemeWhitelist = [
     setting: 'instagram',
     re: /https?:\/\/(?:i.|www.|)instagr(?:.am|am.com)\/p\/.+/,
     default: true,
+    callback: url => {
+      if (!url.endsWith('/')) {
+        url += '/';
+      }
+
+      return fetch(`${getEnpointFor('instagram')}${url}`).then(statusAndJson)
+      .then(data => {
+        if (data.html.includes('video')) {
+          return {
+            type: 'video',
+            thumbnail_url: getSafeURL(data.thumbnail_url),
+            html: `<iframe src="${url}embed/" width="612" height="710" style="max-height: 710px !important;" frameborder="0" scrolling="no" allowtransparency="true"></iframe>`,
+            url,
+          };
+        }
+
+        return {
+          type: 'image',
+          thumbnail_url: getSafeURL(data.thumbnail_url),
+          url: getSafeURL(data.thumbnail_url),
+        };
+      });
+    },
   },
   {
     name: 'Mixcloud',
