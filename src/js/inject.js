57c07892bf017de2dd7f0be9c7aa0827471d74b7
Damien Erambert <damien.erambert@me.com>
2017-02-18 14:55:13 -0800

    :shirt:

--- src/js/inject.js
+++ src/js/inject.js
@@ -376,23 +376,23 @@ document.addEventListener('paste', ev => {
 // $('body').on('click', 'article video.js-media-gif', (ev) => {
 //   ev.preventDefault();
 //   ev.stopPropagation();
-// 
+//
 //   const chirpKey = ev.target.closest('[data-key]').getAttribute('data-key');
 //   const chirpWithVideoId = ev.target.closest('[data-key]').getAttribute('data-key');
 //   const colKey = ev.target.closest('.js-column').getAttribute('data-column');
 //   const video = {
 //     src: ev.target.src,
 //   };
-// 
+//
 //   let chirpObject = getChirpFromKey(chirpKey, colKey);
 //   const chirpsToScan = [chirpObject, chirpObject.quotedTweet, chirpObject.retweetedStatus];
 //   chirpObject = chirpsToScan.find(c => c.id === chirpWithVideoId);
 //   console.log(chirpObject);
-// 
+//
 //   // video.height = (chirpObject.entities.media || chirpObject.quotedTweet.media)[0].sizes.large.h;
 //   // video.width = (chirpObject.entities.media || chirpObject.quotedTweet.media)[0].sizes.large.w;
 //   // video.name = (chirpObject.quotedTweet || chirpObject).user.screenName + '-' + video.src.split('/').pop().replace('.mp4', '');
-// 
+//
 //   proxyEvent('clickedOnGif', { tweetKey: chirpKey, colKey, video });
 // });
 
