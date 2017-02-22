7af1bed2fd86bee7734f839827d030c9556ef382
TAKAHASHI Shuuji <shuuji3@gmail.com>
2017-02-22 12:25:59 +0900

    update the tumblr provider

--- src/js/util/providers/tumblr.js
+++ src/js/util/providers/tumblr.js
@@ -2,12 +2,25 @@ export default function ($) {
   return {
     name: 'Tumblr',
     setting: 'tumblr',
-    re: /tumblr.com\/.+.(?:gif|png|jpg)$/,
+    re: /(?:tumblr.com|tmblr.co)/,
     default: true,
-    callback: url => Promise.resolve({
-      type: 'image',
-      thumbnail_url: $.getSafeURL(url),
-      url: $.getSafeURL(url),
-    }),
+    callback: url => {
+      return fetch(
+        $.getSafeURL(`${$.getEnpointFor('embedly')}key=${$.getKeyFor('embedly')}&url=${url}`))
+        .then($.statusAndJson)
+        .then(json => {
+          if (json.type !== 'photo') {
+            return undefined;
+          }
+          const thumbnailUrl = json.thumbnail_url.replace(json.thumbnail_width, '500').replace('http:', 'https:');
+          const imgUrl = json.url.replace('http:', 'https:');
+          const obj = {
+            type: 'image',
+            thumbnail_url: $.getSafeURL(thumbnailUrl),
+            url: $.getSafeURL(imgUrl),
+          };
+          return obj;
+        });
+    },
   };
 }
