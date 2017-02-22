35be511c6e946235a717f2734792c0e34050d756
TAKAHASHI Shuuji <shuuji3@gmail.com>
2017-02-22 13:08:16 +0900

    add video support

--- src/js/util/providers/tumblr.js
+++ src/js/util/providers/tumblr.js
@@ -9,17 +9,25 @@ export default function ($) {
         $.getSafeURL(`${$.getEnpointFor('embedly')}key=${$.getKeyFor('embedly')}&url=${url}`))
         .then($.statusAndJson)
         .then(json => {
-          if (json.type !== 'photo') {
-            return undefined;
+          if (json.type === 'photo') {
+            const thumbnailUrl = json.thumbnail_url.replace(json.thumbnail_width, '500').replace('http:', 'https:');
+            const imgUrl = json.url.replace('http:', 'https:');
+            const obj = {
+              type: 'image',
+              thumbnail_url: $.getSafeURL(thumbnailUrl),
+              url: $.getSafeURL(imgUrl),
+            };
+            return obj;
+          } else if (json.type === 'video') {
+            const obj = {
+              type: 'video',
+              thumbnail_url: $.getSafeURL(json.thumbnail_url),
+              html: json.html,
+              url,
+            };
+            return obj;
           }
-          const thumbnailUrl = json.thumbnail_url.replace(json.thumbnail_width, '500').replace('http:', 'https:');
-          const imgUrl = json.url.replace('http:', 'https:');
-          const obj = {
-            type: 'image',
-            thumbnail_url: $.getSafeURL(thumbnailUrl),
-            url: $.getSafeURL(imgUrl),
-          };
-          return obj;
+          return undefined;
         });
     },
   };
