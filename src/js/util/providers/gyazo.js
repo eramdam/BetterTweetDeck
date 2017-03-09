f45c4933faab923bc03f5718b7398123ef58f09e
Damien Erambert <damien.erambert@me.com>
2017-03-09 12:31:18 +0100

    :bug: :bulb: support videos for gyazo

--- src/js/util/providers/gyazo.js
+++ src/js/util/providers/gyazo.js
@@ -1,3 +1,6 @@
+import domify from 'domify';
+import { fetch as fetchPage } from '../fetchPage.js';
+
 export default function ($) {
   return {
     name: 'Gyazo',
@@ -5,16 +8,40 @@ export default function ($) {
     re: /gyazo.com/,
     default: true,
     callback: url => {
+      let gyazoData = {};
+
       return fetch($.getSafeURL(`${$.getEnpointFor('gyazo')}${url}`))
         .then($.statusAndJson)
         .then(data => {
-          const obj = {
-            type: 'image',
-            thumbnail_url: $.getSafeURL(data.url),
-            url: $.getSafeURL(data.url),
-          };
+          if (data.type === 'photo') {
+            return {
+              type: 'image',
+              thumbnail_url: $.getSafeURL(data.url),
+              url: $.getSafeURL(data.url),
+            };
+          }
 
-          return obj;
+          gyazoData = data;
+          return fetchPage(url);
+        })
+        .then(data => {
+          if (data.type === 'image') {
+            return data;
+          }
+
+          if (data.currentTarget.status !== 200) {
+            return null;
+          }
+
+          const el = domify(data.currentTarget.response);
+          const thumbnail = el.querySelector('[name="twitter:image"]').content;
+
+          return {
+            type: 'video',
+            thumbnail_url: $.getSafeURL(thumbnail),
+            html: gyazoData.html,
+            url,
+          };
         });
     },
   };
