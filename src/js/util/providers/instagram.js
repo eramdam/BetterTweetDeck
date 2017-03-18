21c9f8c6fc5d05ea1859aa8c1d4d7d39468012ee
Damien Erambert <damien.erambert@me.com>
2017-03-18 16:42:15 +0100

    :bug: update the Instagram provider to correctly support posts with multiple images

--- src/js/util/providers/instagram.js
+++ src/js/util/providers/instagram.js
@@ -11,10 +11,15 @@ export default function ($) {
 
       return fetch(`${$.getEnpointFor('instagram')}${url}`).then($.statusAndJson)
       .then(data => {
-        if (data.html.includes('video')) {
+        // Instagram didn't update their API yet to take accout of posts with multiple medias
+        // so the `thumbnail_url` can give us an empty grey image in those cases.
+        // That's why we want to detect that and use the /media endpoint instead as a fallback
+        const needNewThumbnail = data.thumbnail_url.includes('null.');
+
+        if (data.type === 'rich') {
           return {
             type: 'video',
-            thumbnail_url: $.getSafeURL(data.thumbnail_url),
+            thumbnail_url: $.getSafeURL(needNewThumbnail ? `${url}media/?size=t` : data.thumbnail_url),
             html: `<iframe src="${url}embed/" width="612" height="710" style="max-height: 710px !important;" frameborder="0" scrolling="no" allowtransparency="true"></iframe>`,
             url,
           };
