d9076a03f50b7724350f0fb883ea6a3c5fcbf8c5
Damien Erambert <damien.erambert@me.com>
2017-03-18 17:23:07 +0100

    Fix sizing of images inside of custom modals (#134)
    * :construction: working on new modal markup for cleaner resize/code
    
    * :construction: working on new modal markup for cleaner resize/code
    
    * :wrench: rename alternative template

--- src/js/util/templates.js
+++ src/js/util/templates.js
@@ -48,6 +48,25 @@ const templates = {
       </div>
     </div>
   </div>`,
+  imageModal: `<div class="js-mediatable ovl-block is-inverted-light" btd-custom-modal>
+    <div class="s-padded">
+      <div class="js-modal-panel mdl s-full med-fullpanel"> <a href="#" class="mdl-dismiss js-dismiss mdl-dismiss-media mdl-btn-media" rel="dismiss"><i class="icon txt-size--24 icon-close"></i></a>
+        <div class="js-embeditem med-embeditem btd-embed-container">
+          <div class="btd-embed-and-links">
+            {{^isVideo}}
+            <img class="media-img" src="{{imageUrl}}" alt="{{AltInfo}}" data-btdsetmax>
+            {{/isVideo}}
+            {{#isVideo}}
+              {{&videoEmbed}}
+            {{/isVideo}}
+          </div>
+          
+        </div>
+        <div id="media-gallery-tray"></div>
+        <div class="js-med-tweet med-tweet">{{&tweetEmbed}}</div>
+      </div>
+    </div>
+  </div>`,
 };
 
 const defaultData = {
@@ -107,7 +126,7 @@ const modalTemplate = ({ imageUrl, originalUrl, type, videoEmbed = null, provide
     imageUrl = parsed.searchParams.get('url');
   }
 
-  return Mustache.render(templates.modal, Object.assign(defaultData.modal, {
+  return Mustache.render(type === 'image' ? templates.imageModal : templates.modal, Object.assign(defaultData.modal, {
     imageUrl,
     videoEmbed,
     originalUrl,
