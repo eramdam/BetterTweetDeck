14a277bf176129a2a8dca4263230e6b3d43da88c
Damien Erambert <damien.erambert@me.com>
2017-03-22 18:40:05 +0100

    :bug:

--- src/js/util/providers/universal.js
+++ src/js/util/providers/universal.js
@@ -2,7 +2,7 @@ export default function ($) {
   return {
     name: 'Universal',
     setting: 'universal',
-    re: /.(jpg|gif|png|jpeg)$/,
+    re: /\.(jpg|gif|png|jpeg)$/,
     default: true,
     callback: url => Promise.resolve({
       type: 'image',
