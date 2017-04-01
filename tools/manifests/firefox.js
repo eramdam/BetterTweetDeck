c7636dd0c629d685a64d3427d4456360f42fd362
Damien Erambert <damien.erambert@me.com>
2017-03-31 17:48:13 -0700

    :wrench: add a manifest file for Firefox

---
+++ tools/manifests/firefox.js
@@ -0,0 +1,6 @@
+module.exports = Object.assign(require('./common.js'), {
+  name: '__MSG_appName__',
+  description: '__MSG_appDesc__',
+  default_locale: 'en',
+  'applications.gecko.id': 'BetterTweetDeck@erambert.me',
+});
