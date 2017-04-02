2d6699cac2747280319644fe297adeb2ef8bbb2e
Damien Erambert <damien.erambert@me.com>
2017-04-01 19:07:17 -0700

    :bug: fix firefox's manifest

--- tools/manifests/firefox.js
+++ tools/manifests/firefox.js
@@ -3,6 +3,11 @@ module.exports = Object.assign(require('./common.js'), {
   name: '__MSG_appName__',
   description: '__MSG_appDesc__',
   default_locale: 'en',
-  'applications.gecko.id': 'BetterTweetDeck@erambert.me',
+  applications: {
+    gecko: {
+      id: 'BetterTweetDeck@erambert.me',
+      strict_min_version: '48.0',
+    },
+  },
   content_security_policy: `img-src https: data: 'self' *; media-src * https:; connect-src * https:; style-src 'unsafe-inline'; object-src 'self'; script-src 'self';`,
 });
