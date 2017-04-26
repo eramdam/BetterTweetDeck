2f125e228568ca0d3a476e588417c020f0f0bbae
Damien Erambert <damien.erambert@me.com>
2017-04-25 22:47:39 -0700

    :package: use config w/ pack.js

--- tools/pack.js
+++ tools/pack.js
@@ -1,14 +1,15 @@
 const chalk = require('chalk');
+const config = require('config');
 const clog = (color, ...args) => console.log(chalk[color](...args));
 const fs = require('fs');
 const path = require('path');
 
-if (!process.argv[2]) {
+if (!config.get('opera_key')) {
   clog('red', 'Please provide a private key file');
   process.exit();
 }
 
-const privateKeyPath = path.resolve(process.argv[2]);
+const privateKeyPath = path.resolve(config.get('opera_key'));
 const extensionPath = path.resolve(__dirname, '../dist');
 const ChromeExtension = require('crx');
 const crx = new ChromeExtension({
@@ -23,14 +24,14 @@ crx.load(extensionPath)
     clog('blue', 'Loaded', d.manifest.short_name, d.manifest.version);
 
     return crx.pack().then(buffer => {
-      fs.writeFile(path.resolve(__dirname, '../packed/', 'better-tweetdeck.crx'), buffer, (err) => {
+      fs.writeFile(path.resolve(__dirname, '../artifacts/', 'better-tweetdeck.crx'), buffer, (err) => {
         if (err) {
           clog('red', err);
         }
 
         clog('green', 'Saved better-tweetdeck.crx');
       });
-      fs.writeFile(path.resolve(__dirname, '../packed/', 'better-tweetdeck.nex'), buffer, (err) => {
+      fs.writeFile(path.resolve(__dirname, '../artifacts/', 'better-tweetdeck.nex'), buffer, (err) => {
         if (err) {
           clog('red', err);
         }
