216714f1d3933f019b4b8b804bef8a60fbf83db7
Damien Erambert <damien.erambert@me.com>
2017-10-26 20:54:17 -0700

    Copy static files

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
 const path = require('path');
 const fs = require('fs');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const config = require('config');
 
 fs.writeFileSync(path.resolve(__dirname, 'dist/config.json'), JSON.stringify(config));
+
+const staticFiles = [
+  'icons/*.png',
+  'fonts/*',
+  'emojis/sheet_twitter_64.png',
+  'options/**/*.html',
+  'options/ui/*',
+  'options/img/*',
+  '_locales/**/*',
+].map(i => ({
+  from: i,
+  context: './src/',
+}));
 
 module.exports = {
   entry: {
@@ -30,4 +44,7 @@ module.exports = {
       config: path.resolve(__dirname, 'dist/config.json'),
     },
   },
+  plugins: [
+    new CopyWebpackPlugin(staticFiles),
+  ],
 };
