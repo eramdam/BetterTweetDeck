58719d25cfd467611a8c3e03be08cc11a435d120
Damien Erambert <damien.erambert@me.com>
2017-10-26 21:09:20 -0700

    :construction: working per-browser config/manifest

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
+/* eslint global-require: 0 */
 const path = require('path');
 const fs = require('fs');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const GenerateJsonPlugin = require('generate-json-webpack-plugin');
 const config = require('config');
 
 fs.writeFileSync(path.resolve(__dirname, 'dist/config.json'), JSON.stringify(config));
@@ -19,32 +22,43 @@ const staticFiles = [
   context: './src/',
 }));
 
-module.exports = {
-  entry: {
-    inject: './src/js/inject.js',
-    background: './src/js/background.js',
-    content: './src/js/content.js',
-    'options/options': './src/options/options.js',
-  },
-  output: {
-    filename: '[name].js',
-    path: `${__dirname}/output`,
-  },
-  module: {
-    rules: [
-      {
-        test: /.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
-    ],
-  },
-  resolve: {
-    alias: {
-      config: path.resolve(__dirname, 'dist/config.json'),
+module.exports = (env) => {
+  if (!env || !env.browser) {
+    throw new Error(`Please provide a browser!
+    Possible values: chrome, firefox`);
+  }
+
+  const getManifest = () => require(`./tools/manifests/${env.browser}.js`);
+
+  return {
+    entry: {
+      inject: './src/js/inject.js',
+      background: './src/js/background.js',
+      content: './src/js/content.js',
+      'options/options': './src/options/options.js',
     },
-  },
-  plugins: [
-    new CopyWebpackPlugin(staticFiles),
-  ],
+    output: {
+      filename: '[name].js',
+      path: `${__dirname}/output`,
+    },
+    module: {
+      rules: [
+        {
+          test: /.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        },
+      ],
+    },
+    resolve: {
+      alias: {
+        config: path.resolve(__dirname, 'dist/config.json'),
+      },
+    },
+    plugins: [
+      new CleanWebpackPlugin(['output']),
+      new CopyWebpackPlugin(staticFiles),
+      new GenerateJsonPlugin('manifest.json', getManifest(), null, 2),
+    ],
+  };
 };
