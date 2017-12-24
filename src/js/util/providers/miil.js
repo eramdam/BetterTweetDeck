7fa4e2f68fcdf88ef0b384dbb05ef5a70eb0038b
TAKAHASHI Shuuji <shuuji3@gmail.com>
2017-12-24 14:56:02 +0900

    Replace 'https' by 'http'

--- src/js/util/providers/miil.js
+++ src/js/util/providers/miil.js
@@ -1,14 +1,17 @@
-export default function ($) {
+export default function($) {
   return {
     name: 'Miil',
     setting: 'miil_me',
     re: /miil\.me\/p\/[a-zA-Z0-9]+$/,
     default: true,
-    callback: url => Promise.resolve({
-      type: 'image',
-      thumbnail_url: $.getSafeURL($.getSafeURL(`${url}.jpeg`)),
-      url: $.getSafeURL($.getSafeURL(`${url}.jpeg`)),
-    }),
+    callback: (url) => {
+      const imgUrl = $.getSafeURL(`${url.replace('https://', 'http://')}.jpeg`);
+      return Promise.resolve({
+        type: 'image',
+        thumbnail_url: imgUrl,
+        url: imgUrl,
+      });
+    },
   };
 }
 
