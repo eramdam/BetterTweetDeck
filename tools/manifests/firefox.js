f989af838f723172bf49ef859ea85147dd203705
Damien Erambert <damien.erambert@me.com>
2018-01-06 13:13:39 -0800

    :bulb: refactor the common URLs permissions into one file and add those who were missing

--- tools/manifests/firefox.js
+++ tools/manifests/firefox.js
@@ -1,4 +1,5 @@
 const config = require('config');
+const urls = require('./commonHosts');
 
 /* eslint quotes: 0 */
 module.exports = Object.assign(require('./common.js'), {
@@ -13,31 +14,10 @@ module.exports = Object.assign(require('./common.js'), {
   },
   permissions: [
     'storage',
-    'https://api.embed.ly/1/*',
-    'https://backend.deviantart.com/*',
-    'https://500px.com/oembed*',
-    'https://api.dailymotion.com/video/*',
-    'https://api.dribbble.com/v1/shots/*',
-    'https://*.imgur.com/*',
-    'https://*.instagram.com/*',
-    'https://images4-focus-opensocial.googleusercontent.com/gadgets/proxy*',
-    'http://erambert.me/*',
-    'https://embed.spotify.com/oembed/*',
-    'https://open.spotify.com/oembed/*',
-    'https://api.streamable.com/*',
-    'https://api.twitch.tv/kraken/*',
-    'https://clips.twitch.tv/*',
-    'https://*.bandcamp.com/*',
-    'https://cl.ly/*',
-    'https://giphy.com/services/oembed*',
-    'https://api.gyazo.com/api/*',
-    'https://www.tinami.com/api/*',
-    'https://gyazo.com/*',
-    '*://tweetdeck.twitter.com/*',
-    'https://raw.githubusercontent.com/eramdam/BetterTweetDeck/master/*',
     'contextMenus',
     'notifications',
     'tabs',
+    ...urls,
   ],
   content_security_policy: `img-src https: data: 'self' *; media-src * https:; connect-src * https:; style-src 'unsafe-inline'; object-src 'self'; script-src 'self';`,
 });
