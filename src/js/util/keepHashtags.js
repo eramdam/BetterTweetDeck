1dc584ee6270b38e052da713763787052a97a166
zenmaibane <koteicream.1024@gmail.com>
2018-02-19 00:51:06 +0900

    make keep-hashtag func better

--- src/js/util/keepHashtags.js
+++ src/js/util/keepHashtags.js
@@ -3,14 +3,15 @@ const keepHashtags = () => {
   let hashtags = [];
   const tweetObserver = new MutationObserver(() => {
     if (tweetTextArea.disabled) {
-      hashtags = [];
       const tweetedHashtags = tweetTextArea.value.match(/[\s][#＃][Ａ-Ｚａ-ｚA-Za-z一-鿆0-9０-９ぁ-ヶｦ-ﾟー_]+/g);
-      if (tweetedHashtags) {
-        for (let i = 0; i < tweetedHashtags.length; i += 1) {
-          tweetedHashtags[i] = tweetedHashtags[i].substr(1);
-        }
-        hashtags = tweetedHashtags;
+      if (!tweetedHashtags) {
+        hashtags = [];
+        return;
       }
+      for (let i = 0; i < tweetedHashtags.length; i += 1) {
+        tweetedHashtags[i] = tweetedHashtags[i].substr(1);
+      }
+      hashtags = tweetedHashtags;
     } else {
       if (hashtags.length !== 0) {
         tweetTextArea.value = ` ${hashtags.join(' ')}`;
