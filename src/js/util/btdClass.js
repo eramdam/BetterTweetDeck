86e8977dc9df7040f97c6d607a033991665fa8eb
Damien Erambert <damien.erambert@me.com>
2018-04-08 20:14:04 -0700

    :wrench:

--- src/js/util/btdClass.js
+++ src/js/util/btdClass.js
@@ -1,5 +1,9 @@
 export class BTDComponent {
   constructor(settings) {
+    if (!settings) {
+      throw new Error('Please pass Better TweetDeck\'s settings to the constructor');
+    }
+
     this.settings = settings;
   }
 }
