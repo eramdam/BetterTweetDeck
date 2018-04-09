fae7a89f6382c4b572a32300e32d6630da7984a6
Damien Erambert <damien.erambert@me.com>
2018-04-08 20:25:49 -0700

    :memo: add some doc

--- src/js/inject.js
+++ src/js/inject.js
@@ -4,11 +4,35 @@ import { Time } from './util/time';
 const BTD_SETTINGS = $('[data-btd-settings]').data('btd-settings');
 const BTDTime = new Time(BTD_SETTINGS);
 
-$(document).one('dataColumnsLoaders', () => {
-  console.log('ready!');
-});
-
-
+/*
+ * If the user chose so, we override the timestamp function called by TweetDeck
+ */
 if (BTD_SETTINGS.ts !== 'relative') {
   TD.util.prettyDate = d => BTDTime.prettyDate(d);
 }
+
+/*
+ * If the user chose so, we override the link creation mechanism to remove the t.co redirection
+ */
+if (BTD_SETTINGS.no_tco) {
+  const dummyEl = document.createElement('span');
+  const originalCreateUrlAnchor = TD.util.createUrlAnchor;
+
+  TD.util.createUrlAnchor = (e) => {
+    let result = originalCreateUrlAnchor(e);
+    // this never touches the final DOM ever so it's fine
+    dummyEl.innerHTML = result;
+    const anchor = dummyEl.querySelector('a');
+
+    if (anchor) {
+      anchor.href = anchor.dataset.fullUrl;
+      result = anchor.outerHTML;
+    }
+
+    return result;
+  };
+}
+
+$(document).one('dataColumnsLoaders', () => {
+  console.log('ready!');
+});
--- src/js/util/time.js
+++ src/js/util/time.js
@@ -14,9 +14,14 @@ const formatMaps = {
 };
 
 export class Time extends BTDComponent {
+  /** Computes whether or not the passed Date object is less than 24h old or not */
   lessThan24 = d => new Date().getTime() - d.getTime() <= 60 * 60 * 24000;
 
+  /**
+   * Returns the right formatting string according to BTD's settings
+   */
   getFormat = (d) => {
+    // If the mode defined is "custom" then we use the formatting strings defined by the user
     if (this.settings.ts === 'custom') {
       if (this.settings.full_after_24 && this.lessThan24(d)) {
         return this.settings.custom_ts.short;
@@ -25,11 +30,17 @@ export class Time extends BTDComponent {
       return this.settings.custom_ts.full;
     }
 
+    // We use the formats corresponding to the current timestamp setting
     if (this.settings.full_after_24 && this.lessThan24(d)) {
       return formatMaps[this.settings.ts].short;
     }
 
     return formatMaps[this.settings.ts].full;
   };
+
+  /**
+   * Returns a "pretty" (absolute) time string based on BTD's settings
+   * @param {Date} d Date object
+   */
   prettyDate = d => format(d, this.getFormat(d));
 }
