33b9426ff12273c1054a8e5c12279ddbbf3f5843
Damien Erambert <damien.erambert@me.com>
2018-04-23 20:51:12 -0700

    :sparkles: make the content script inside an async function :fire:

--- src/js/content.js
+++ src/js/content.js
@@ -1,11 +1,16 @@
-import { getExtensionUrl, settings } from './util/browserHelpers';
+import { getExtensionUrl, settings as extensionSettings } from './util/browserHelpers';
 
-settings.get().then((data) => {
-  console.log({ data });
+(async () => {
+  /**
+   * Get the extensions settings from the browser
+   */
+  const settings = await extensionSettings.get();
+
+  /**
+   * Inject our script into the page
+   */
   const injected = document.createElement('script');
   injected.src = getExtensionUrl('js/inject.js');
-
-  injected.dataset.btdSettings = JSON.stringify(data);
-
+  injected.dataset.btdSettings = JSON.stringify(settings);
   document.head.appendChild(injected);
-});
+})();
