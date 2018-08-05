562122505744a06d7d32225c10eed8ef26d3c0b9
Damien Erambert <damien.erambert@me.com>
2018-08-05 15:44:43 -0700

    :shirt:

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
   extends: ['airbnb'],
   env: {
     browser: true,
-    node: true,
+    node: true
   },
   rules: {
     'no-underscore-dangle': 0,
@@ -16,20 +16,23 @@ module.exports = {
     'import/prefer-default-export': 0,
     'import/order': 0,
     'jsx-a11y/anchor-has-content': 0,
-    'react/jsx-no-target-blank': 0
+    'react/jsx-no-target-blank': 0,
+    'max-len': [2, {code: 200}]
   },
-  overrides: [{
-    files: ['*.ts', '*.tsx'],
-    rules: {
-      'import/no-unresolved': 0,
-      'no-unused-vars': 0,
-      'no-undef': 0,
-      'react/jsx-filename-extension': 0,
-      'react/jsx-one-expression-per-line': 0,
-      'react/destructuring-assignment': 0,
-      'react/prop-types': 0,
-      'react/prefer-stateless-function': 0,
-      'no-restricted-globals': 0,
-    },
-  }],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'import/no-unresolved': 0,
+        'no-unused-vars': 0,
+        'no-undef': 0,
+        'react/jsx-filename-extension': 0,
+        'react/jsx-one-expression-per-line': 0,
+        'react/destructuring-assignment': 0,
+        'react/prop-types': 0,
+        'react/prefer-stateless-function': 0,
+        'no-restricted-globals': 0
+      }
+    }
+  ]
 };
