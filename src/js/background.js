0df2df000bcba277af7681f3aeedba75439eb53e
Damien Erambert <damien.erambert@me.com>
2019-07-10 20:53:35 -0700

    Modify TweetDeck's CSP to fix fetch errors (#424)
    * very crude removal of CSP
    
    * :bulb: better way that doesn't involve harcoding existing CSP
    
    * only touch tweetdeck
    
    * better pattern for twimg

--- src/js/background.js
+++ src/js/background.js
@@ -242,3 +242,58 @@ Messages.on((message, sender, sendResponse) => {
       return false;
   }
 });
+
+function addUrlsToCSP(headerValue, directive, ...values) {
+  return String(headerValue)
+    .split('; ')
+    .map((line) => {
+      const [directiveName, ...patterns] = line.split(' ');
+      const newPatterns =
+        directive === directiveName ? patterns.concat(values) : patterns;
+
+      return [directiveName, ...newPatterns].join(' ');
+    })
+    .join('; ');
+}
+
+chrome.permissions.contains(
+  {
+    permissions: ['webRequest', 'webRequestBlocking'],
+  },
+  (hasWR) => {
+    if (!hasWR) {
+      return;
+    }
+
+    chrome.webRequest.onHeadersReceived.addListener(
+      (details) => {
+        if (details.type !== 'main_frame' && details.type !== 'sub_frame') {
+          return undefined;
+        }
+
+        return {
+          responseHeaders: Array.from(details.responseHeaders).map((h) => {
+            if (h.name && h.name === 'content-security-policy') {
+              const mod = Object.assign(h, {
+                value: addUrlsToCSP(
+                  h.value,
+                  'connect-src',
+                  'https://*.tenor.com',
+                  'https://*.giphy.com',
+                  'https://*.twimg.com',
+                ),
+              });
+              return mod;
+            }
+
+            return h;
+          }),
+        };
+      },
+      {
+        urls: ['https://tweetdeck.twitter.com/*'],
+      },
+      ['responseHeaders', 'blocking'],
+    );
+  },
+);
