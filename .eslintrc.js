c5d52e6ba2bb57d637bba2bcc655ffe300e58ee2
Damien Erambert <damien.erambert@me.com>
2019-09-23 20:36:09 -0700

    add TS

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
-  plugins: ['prettier'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
   extends: ['airbnb', 'prettier'],
   rules: {
     'prettier/prettier': 'error',
