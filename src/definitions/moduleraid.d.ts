15d284ce69d571add1fb2230aa9d5d5a65da862f
Damien Erambert <damien.erambert@me.com>
2019-09-23 20:11:59 -0700

    :fire:

--- src/definitions/moduleraid.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'moduleraid' {
-  export default function moduleraid(): {
-    modules: any[];
-    constructors: any[];
-    get: (id: string) => any;
-    findModule: (query: string) => any;
-    findFunction: (query: string) => any[];
-  };
-}
