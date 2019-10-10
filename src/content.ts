f19d08015f565e688894bb5fc35bcc2eb0a3cef5
Damien Erambert <damien.erambert@me.com>
2019-10-09 22:32:30 -0700

    bleh

--- src/content.ts
+++ src/content.ts
@@ -1,3 +1,59 @@
-import {injectInTD} from './services/injectInTD';
+import {uniqueId} from 'lodash';
 
-injectInTD();
+import {injectInTD} from './services/injectInTD';
+import {TweetDeckObject} from './types/tweetdeckTypes';
+
+(async () => {
+  await injectInTD();
+})();
+
+document.addEventListener(
+  'click',
+  async (ev) => {
+    const {target} = ev;
+
+    if (!(target instanceof HTMLElement)) {
+      return;
+    }
+
+    if (!target.matches('article.stream-item') && !target.closest('article.stream-item')) {
+      return;
+    }
+
+    const article = target.closest('article.stream-item');
+    if (!article) {
+      return;
+    }
+
+    const chirpKey = article.getAttribute('data-key') || '';
+    const chirpContainer = article.closest('[data-column]');
+    const colKey = (chirpContainer && chirpContainer.getAttribute('data-column')) || '';
+
+    console.log({chirpKey, colKey});
+  },
+  {capture: true}
+);
+
+function getTweetDeckObjectFromInject(): Promise<TweetDeckObject> {
+  const requestId = uniqueId();
+  console.log('getTweetDeckObjectFromInject', {requestId});
+
+  return new Promise((resolve) => {
+    const listener = (ev: MessageEvent) => {
+      const {origin, data} = ev;
+
+      if (['tweetdeck.twitter.com', 'https://better.tw'].some((o) => origin.includes(o))) {
+        const {name} = data;
+
+        if (name === 'TD_BACK' && data.requestId === requestId) {
+          console.log({data, requestId}, JSON.parse(data.data));
+          window.removeEventListener('message', listener);
+          resolve(JSON.parse(data.data));
+        }
+      }
+    };
+
+    window.addEventListener('message', listener);
+    window.postMessage({name: 'GET_TD', requestId}, 'https://tweetdeck.twitter.com');
+  });
+}
