50374ec9513052d542602f5fcecd9316da0252fd
Damien Erambert <damien.erambert@me.com>
2020-01-19 15:29:51 -0800

    scrollbar wip

--- src/features/changeScrollbars.ts
+++ src/features/changeScrollbars.ts
@@ -1,5 +1,13 @@
+import './changeScrollbars.css';
+
+import {BTDSettings} from '../types/betterTweetDeck/btdSettingsTypes';
+
 export enum BTDScrollbarsMode {
   DEFAULT = 'default',
   SLIM = 'slim',
   HIDDEN = 'hidden',
+}
+
+export function changeScrollbarStyling(settings: BTDSettings) {
+  document.body.setAttribute('btd-scrollbar-style', settings.scrollbarsMode);
 }
