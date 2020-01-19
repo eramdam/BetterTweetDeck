47c16afe0e168a70a26cbe72d6b8fc6b5e191ab4
Damien Erambert <damien.erambert@me.com>
2020-01-19 15:19:50 -0800

    timestamp formatting working

--- src/features/changeTimestampFormat.ts
+++ src/features/changeTimestampFormat.ts
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-
+import {DateTime, Interval} from 'luxon';
 import {BTDSettings} from '../types/betterTweetDeck/btdSettingsTypes';
 import {TweetDeckObject} from '../types/tweetdeckTypes';
 
@@ -8,8 +8,50 @@ export enum BTDTimestampFormats {
   ABSOLUTE = 'absolute',
 }
 
-function removeOriginalTask(TD: TweetDeckObject, chosenFormat: BTDTimestampFormats) {
-  if (chosenFormat === BTDTimestampFormats.RELATIVE) {
+const TIMESTAMP_INTERVAL = 1e3 * 8;
+
+function getFinalDateString(
+  sourceIsoString: string,
+  {timestampFullFormat, timestampShortFormat, fullTimestampAfterDay}: BTDSettings
+) {
+  const dateObject = DateTime.fromISO(sourceIsoString);
+  const now = DateTime.local();
+  const fullString = dateObject.toFormat(timestampFullFormat);
+  const shortString = dateObject.toFormat(timestampShortFormat);
+
+  if (fullTimestampAfterDay) {
+    const intervalToNow = Interval.fromDateTimes(dateObject, now).length('hours');
+
+    return intervalToNow > 24 ? fullString : shortString;
+  }
+
+  return shortString;
+}
+
+function refreshTimestamps(settings: BTDSettings) {
+  const timeElements = document.querySelectorAll('time.js-timestamp');
+
+  timeElements.forEach((timeElement) => {
+    const timeString = timeElement.getAttribute('datetime');
+    if (!timeString) {
+      return;
+    }
+
+    const textLikeElement = timeElement.querySelector('a, span');
+    const newTextContent = getFinalDateString(timeString, settings);
+
+    if (!textLikeElement) {
+      timeElement.textContent = newTextContent;
+      return;
+    }
+
+    textLikeElement.textContent = newTextContent;
+  });
+}
+
+export function maybeSetupCustomTimestampFormat(TD: TweetDeckObject, settings: BTDSettings) {
+  const {timestampStyle} = settings;
+  if (timestampStyle === BTDTimestampFormats.RELATIVE) {
     return;
   }
 
@@ -33,8 +75,6 @@ function removeOriginalTask(TD: TweetDeckObject, chosenFormat: BTDTimestampForma
 
   // Otherwise, remove the periodic task
   TD.controller.scheduler.removePeriodicTask(taskIdToRemove);
-}
-
-export function maybeSetupCustomTimestampFormat(TD: TweetDeckObject, settings: BTDSettings) {
-  removeOriginalTask(TD, settings.timestampStyle);
+  refreshTimestamps(settings);
+  setInterval(() => refreshTimestamps(settings), TIMESTAMP_INTERVAL);
 }
