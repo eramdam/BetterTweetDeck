65bd3ad7bc8ed80642240e09491dc5d3b394fb31
Damien Erambert <damien.erambert@me.com>
2020-01-19 15:30:23 -0800

    add eslint sort plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'react-hooks'],
+  plugins: ['@typescript-eslint', 'react-hooks', 'unused-imports', 'simple-import-sort'],
   extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 2020,
@@ -28,6 +28,8 @@ module.exports = {
     {
       files: ['*.ts', '*.tsx'],
       rules: {
+        'simple-import-sort/sort': 2,
+        'unused-imports/no-unused-imports-ts': 2,
         'no-use-before-define': 0,
         'no-unused-vars': 0,
         'no-undef': 0,
