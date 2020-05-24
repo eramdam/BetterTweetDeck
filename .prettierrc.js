78d335b25e8d2ae111a61edfd9506d8242b97068
Damien Erambert <damien.erambert@me.com>
2020-05-23 22:06:20 -0700

    Update .prettierrc.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,11 @@
 // prettier.config.js or .prettierrc.js
 module.exports = {
-  trailingComma: 'es5',
+  trailingComma: "es5",
   tabWidth: 2,
   semi: true,
   printWidth: 100,
   singleQuote: true,
-  singleQuote: true,
   bracketSpacing: false,
   jsxBracketSameLine: true,
-  arrowParens: 'always',
+  arrowParens: "always",
 };
