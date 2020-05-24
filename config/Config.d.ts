7e60e70e52e0951629d9759c705a7de755168238
Damien Erambert <damien.erambert@me.com>
2020-05-23 22:17:29 -0700

    upgrade deps

--- config/Config.d.ts
+++ config/Config.d.ts
@@ -1,31 +1,35 @@
 /* tslint:disable */
 /* eslint-disable */
-interface Config {
-  FirefoxId: string;
-  opera_key: string;
-  Client: Client;
-}
-interface Client {
-  github_token: string;
-  debug: boolean;
-  remote_inst: boolean;
-  APIs: APIs;
-  extension_ids: Extensionids;
-}
-interface Extensionids {
-  chrome: Chrome;
-  firefox: Chrome;
-  opera: Chrome;
-}
-interface Chrome {
-  release: string;
-  beta: string;
-}
-interface APIs {
-  dribbble: string;
-  imgur: string;
-  twitch: string;
-  tinami: string;
-  giphy: string;
-  tenor: string;
+declare module "node-config-ts" {
+  interface IConfig {
+    FirefoxId: string
+    opera_key: string
+    Client: Client
+  }
+  interface Client {
+    github_token: string
+    debug: boolean
+    remote_inst: boolean
+    APIs: APIs
+    extension_ids: Extensionids
+  }
+  interface Extensionids {
+    chrome: Chrome
+    firefox: Chrome
+    opera: Chrome
+  }
+  interface Chrome {
+    release: string
+    beta: string
+  }
+  interface APIs {
+    dribbble: string
+    imgur: string
+    twitch: string
+    tinami: string
+    giphy: string
+    tenor: string
+  }
+  export const config: Config
+  export type Config = IConfig
 }
