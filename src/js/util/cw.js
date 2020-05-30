6f0b0771bac838e045a6a22d9a526b0fa1e7fb49
Andreas Nedbal <andy@pixelde.su>
2020-05-30 22:31:19 +0200

    Require space after warning

--- src/js/util/cw.js
+++ src/js/util/cw.js
@@ -4,7 +4,7 @@ export default function contentWarningHandler(node, tweet) {
     return false;
   }
 
-  const cwRegex = /^[[(]?(?:cw|tw|cn)(?:\W+)?\s?([^\n|\]|)|…]+)[\])…]?(?:\n+)?((?:.+)?\n?)+$/gi;
+  const cwRegex = /^[[(]?(?:cw|tw|cn)(?:\W+)?\s([^\n|\]|)|…]+)[\])…]?(?:\n+)?((?:.+)?\n?)+$/gi;
   const matches = [...tweet.htmlText.matchAll(cwRegex)];
 
   if (matches.length > 0) {
