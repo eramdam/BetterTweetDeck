bf6ee235e7f22c9cd0369fdb6be34390fce2e4a3
Damien Erambert <damien.erambert@me.com>
2020-06-13 16:36:38 -0700

    simpler fix for dom-chef typings

--- definitions/domChefFix.d.ts
+++ definitions/domChefFix.d.ts
@@ -1,19 +1,3 @@
-// declare namespace JSX {
-//   interface Element extends SVGElement, HTMLElement, DocumentFragment {}
-//   type BaseIntrinsicElement = IntrinsicElements['div'];
-//   type LabelIntrinsicElement = IntrinsicElements['label'];
-//   interface IntrinsicElements {
-//     'has-rgh': BaseIntrinsicElement;
-//     label: LabelIntrinsicElement & {for?: string};
-//     'include-fragment': BaseIntrinsicElement & {src?: string};
-//     'details-menu': BaseIntrinsicElement & {src?: string; preload?: boolean};
-//     'time-ago': BaseIntrinsicElement & {datetime: string; format?: string};
-//     'relative-time': BaseIntrinsicElement & {datetime: string};
-//     'details-dialog': BaseIntrinsicElement & {tabindex: string};
-//   }
-
-//   interface IntrinsicAttributes extends BaseIntrinsicElement {
-//     width?: number;
-//     height?: number;
-//   }
-// }
+declare namespace JSX {
+  interface Element extends SVGElement, HTMLElement, DocumentFragment {}
+}
