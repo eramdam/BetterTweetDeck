0c8f7e512726de0ff27e1658bdc49df1717b94d8
Damien Erambert <damien.erambert@me.com>
2021-01-16 16:28:58 -0800

    add toolbar icons

--- tools/manifests/common.js
+++ tools/manifests/common.js
@@ -59,6 +59,12 @@ const common = {
     scripts: ['build/js/background.js'],
   },
   icons: isBeta ? betaIcons : icons,
+  browser_action: {
+    default_icon: {
+      16: 'build/icons/toolbar-icon-16.png',
+      32: 'build/icons/toolbar-icon-32.png',
+    },
+  },
   permissions: ['storage', 'contextMenus', ...urls],
   options_ui: {
     page: 'build/options/ui/ui.html',
