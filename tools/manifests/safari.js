cf5783342a7b3b07eb88af143f833a79f8e8775f
Damien Erambert <damien.erambert@me.com>
2021-04-03 18:49:40 -0700

    Rename extension on Safari to avoid App Store violation

--- tools/manifests/safari.js
+++ tools/manifests/safari.js
@@ -1,5 +1,6 @@
 const chromeManifest = require('./chrome');
 module.exports = Object.assign(chromeManifest, {
+  name: 'Better TDeck for TweetDeck',
   browser_action: {
     default_icon: {
       16: 'build/assets/icons/toolbar-icon-16.png',
