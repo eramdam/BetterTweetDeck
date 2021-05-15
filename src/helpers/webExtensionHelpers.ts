473ca9e3945944c2ff2a1df044243f6e6d3353c4
Damien Erambert <damien.erambert@me.com>
2021-05-15 15:12:18 -0700

    Dynamically remove context menu item when saving settings
    https://github.com/eramdam/BetterTweetDeck/issues/607

--- src/helpers/webExtensionHelpers.ts
+++ src/helpers/webExtensionHelpers.ts
@@ -1,6 +1,7 @@
 import {browser} from 'webextension-polyfill-ts';
 
 import {BTDMessageEvent, BTDMessageEventData} from '../types/btdMessageTypes';
+import {BTDSettings} from '../types/btdSettingsTypes';
 
 /**
  * Converts a relative path within an extension install directory to a fully-qualified URL.
@@ -9,6 +10,20 @@ export const getExtensionUrl = (url: string) => browser.extension.getURL(url);
 
 /** Exposes the WebExtensions settings object/API. */
 export const ExtensionSettings = browser.storage.sync;
+
+export type StorageChangeHandler = (
+  changes: {
+    [k in keyof BTDSettings]: {
+      oldValue: BTDSettings[k];
+      newValue: BTDSettings[k];
+    };
+  },
+  area: string
+) => void;
+
+export function listenForStorageChange(cb: StorageChangeHandler) {
+  browser.storage.onChanged.addListener(cb as any);
+}
 
 /** Returns the version of the extension. */
 export const getExtensionVersion = () => browser.runtime.getManifest().version;
