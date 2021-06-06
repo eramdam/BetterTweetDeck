8cec412dbb2d82adc27275dae71afd58b1cf5bca
Damien Erambert <damien@erambert.me>
2021-06-06 00:11:34 -0700

    :wrench:

---
+++ tools/manifests/firefox-beta.js
@@ -0,0 +1,14 @@
+const config = require('config');
+const {DateTime} = require('luxon');
+
+module.exports = {
+  ...require('./common.js'),
+  version: DateTime.local().toFormat('yy.M.dHH.mm'),
+  optional_permissions: ['tabs'],
+  applications: {
+    gecko: {
+      id: config.get('FirefoxBetaId'),
+      strict_min_version: '84.0',
+    },
+  },
+};
