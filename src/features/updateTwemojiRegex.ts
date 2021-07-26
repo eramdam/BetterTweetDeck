63d2be5f661af056bc2905f8c327dd7d5b80faa6
Damien Erambert <damien@erambert.me>
2021-07-25 18:07:55 -0700

    Update updateTwemojiRegex.ts

--- src/features/updateTwemojiRegex.ts
+++ src/features/updateTwemojiRegex.ts
@@ -1,14 +1,16 @@
+import {ModuleLike} from 'moduleraid';
 // @ts-expect-error
 import regexp from 'twitter-regexps/emoji';
 
-import {makeIsModuleRaidModule} from '../helpers/typeHelpers';
+import {hasProperty, makeIsModuleRaidModule} from '../helpers/typeHelpers';
 import {makeBTDModule} from '../types/btdCommonTypes';
 
-const twitterModulePredicate = (mod: any) => mod.default instanceof RegExp;
+const twitterModulePredicate = (mod: ModuleLike) =>
+  hasProperty(mod, 'default') && mod.default instanceof RegExp;
 const isTwitterRegexModule = makeIsModuleRaidModule<{default: RegExp}>(twitterModulePredicate);
 
 export const updateTwemojiRegex = makeBTDModule(({mR}) => {
-  const twitterRegexModule = mR.findModule(twitterModulePredicate)[0];
+  const twitterRegexModule = mR.findModule(isTwitterRegexModule)[0];
 
   if (!isTwitterRegexModule(twitterRegexModule)) {
     return;
