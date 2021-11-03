d6849f97274bceaa8ae3cd1345c6d35e6d323a77
Damien Erambert <damien@erambert.me>
2021-11-02 23:05:16 -0700

    upload AMO build to discord channel

---
+++ tools/uploadNightly.ts
@@ -0,0 +1,33 @@
+import {MessageAttachment, WebhookClient} from 'discord.js';
+import * as glob from 'glob';
+import * as path from 'path';
+
+const GITHUB_SHA = (process.env.GITHUB_SHA || '').substr(0, 7);
+const NIGHTLY_DISCORD_WEBHOOK_ID = process.env.NIGHTLY_DISCORD_WEBHOOK_ID || '';
+const NIGHTLY_DISCORD_WEBHOOK_TOKEN = process.env.NIGHTLY_DISCORD_WEBHOOK_TOKEN || '';
+
+(async () => {
+  const webhookClient = new WebhookClient({
+    id: NIGHTLY_DISCORD_WEBHOOK_ID,
+    token: NIGHTLY_DISCORD_WEBHOOK_TOKEN,
+  });
+
+  const xpiFile = glob.sync('+(web-ext-artifacts|artifacts)/*.xpi')[0];
+
+  if (!xpiFile) {
+    process.exit(0);
+  }
+
+  const xpiFilename = path.basename(xpiFile);
+  const versionString = xpiFilename
+    .replace('better_tweetdeck-', '')
+    .replace('-an+fx', '')
+    .replace('.xpi', '');
+
+  const nightlyBuild = new MessageAttachment(xpiFile, `Better TweetDeck ${versionString}.xpi`);
+
+  webhookClient.send({
+    content: `Built version ${versionString} based on [${GITHUB_SHA}](<https://github.com/eramdam/BetterTweetDeck/commit/${GITHUB_SHA}>)`,
+    files: [nightlyBuild],
+  });
+})();
