7f096d11e621858f7b51cf68f521ed8180b79880
Damien Erambert <damien@erambert.me>
2021-11-02 23:06:57 -0700

    always inject debug methods

--- src/services/debugMethods.ts
+++ src/services/debugMethods.ts
@@ -1,12 +1,7 @@
-import {BtdConfig} from '../defineConfig';
 import {findMustache, getChirpFromElement, getChirpFromKey} from '../helpers/tweetdeckHelpers';
 import {makeBTDModule} from '../types/btdCommonTypes';
 
 export const maybeSetupDebugFunctions = makeBTDModule(({jq, mR}) => {
-  if (!BtdConfig.debug) {
-    return;
-  }
-
   window.BTD = {
     debug: {
       getChirpFromElement,
