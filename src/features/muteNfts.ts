552de14d3c03f8eea03eae86061b018e7f5648f1
Damien Erambert <damien@erambert.me>
2021-11-10 16:11:12 -0500

    Update muteNfts.ts

--- src/features/muteNfts.ts
+++ src/features/muteNfts.ts
@@ -6,12 +6,10 @@ export const muteNftAvatars = makeBTDModule(({TD, jq, settings}) => {
       .getAll()
       .filter((f) => f.type === 'BTD_nft_avatar');
 
-    if (nftFilters.length < 1) {
-      return;
-    }
-
     if (settings.muteNftAvatars) {
-      TD.controller.filterManager.addFilter('BTD_nft_avatar', '');
+      if (nftFilters.length < 1) {
+        TD.controller.filterManager.addFilter('BTD_nft_avatar', '');
+      }
     } else {
       nftFilters.forEach((filter) => {
         TD.controller.filterManager.removeFilter(filter);
