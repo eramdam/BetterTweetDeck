066968e79f324a7f1985f313c9face5fe412ca15
Damien Erambert <damien@erambert.me>
2021-11-12 17:53:01 -0500

    :memo:

--- src/features/muteNfts.ts
+++ src/features/muteNfts.ts
@@ -68,44 +68,31 @@ export const muteNftAvatars = makeBTDModule(({TD, jq, settings}) => {
         <div class="js-modal-content modal-content position-rel">
           <div class="padding-b--10 txt-size--16 width--600">
             <h1 class="padding-v--20 txt-size--24 width--430 padding-b--20 margin-a-auto txt-bold">
-              Better TweetDeck stands against NFTs and cryptocurrency.
+              Better TweetDeck stands against NFTs.
             </h1>
             <div class="color-twitter-darker-gray txt-size--15 width--430 margin-a-auto">
               <p>
-                This user chose to <a href="https://twitter.com/TheSmarmyBum/status/1443259893411049475" target="_blank">connect their NFT wallet</a> to their profile, so Better TweetDeck hid their tweet.
+                This user chose to <a href="https://twitter.com/TheSmarmyBum/status/1443259893411049475" target="_blank">use the NFT integration</a> on their profile, so Better TweetDeck hid their tweet.
                 <br /> <br />
-                I am standing up against NFTs and their dangerous rise in popularity.
-                <br /><br />
-                Why is that? Because I firmly believe that NFTs are dangerous for a few reasons
-              </p>
-              <ol>
-                <li>They are bad for the environment, as they rely on cryptocurrencies that cause huge amounts of carbon emissions.</li>
-                <li>They are only valuable as tools for money laundering, tax evasion, and greater fool investment fraud.</li>
-                <li>Their structure facilitates art theft despite being touted as a way to "empower artists"</li>
-              </ol>
-
-              <p>
-                This is why I am using my platform to spread awareness about the dangers of this technology.
-                <br /><br />
-                If you think that "actually, NFTs are good," it's okay. You're free to uninstall Better TweetDeck.
-                <br /><br />
-
-                Otherwise, the two options below are available to you:
+                Twitter as a company can do whatever they want, but I refuse to condone or support this feature, which is why you're seeing this. 
+                <br />
+                You can choose to either mute or show tweets by users who used the Twitter NFT integration:
               </p>
 
               <div class="nft-choices">
-                <button class="Button Button--primary nft-mute">Mute users who use NFT avatars</button>
-                <button class="Button Button--link nft-show">Keep showing NFT users</button>
+                <button class="Button Button--primary nft-mute">Mute users who connected NFT avatars</button>
+                <button class="Button Button--link nft-show">Keep showing them</button>
               </div>
 
-              <p>Sources:</p>
-              <ol>
-                <li><a href="https://antsstyle.medium.com/why-nfts-are-bad-the-long-version-2c16dae145e2" target="_blank">Why NFTs are bad: the long version</a></li>
-                <li><a href="https://earth.org/nfts-environmental-impact/">What Are NFTs, And What is Their Environmental Impact?</a></li>
-                <li><a href="https://twitter.com/NFTtheft">Account documenting art thefts happening in the NFT space</a></li>
-                <li><a href="https://twitter.com/smdiehl/status/1445795667826208770">https://twitter.com/smdiehl/status/1445795667826208770</a></li>
-              </ol>
-
+              <p>
+                For more information, see:
+              </p>
+              <ul>
+                <li><a target="_blank" href="https://antsstyle.medium.com/why-nfts-are-bad-the-long-version-2c16dae145e2">Why NFTs are bad: the long version</a></li>
+                <li><a target="_blank" href="https://earth.org/nfts-environmental-impact/">What Are NFTs, And What is Their Environmental Impact?</a></li>
+                <li><a target="_blank" href="https://twitter.com/NFTtheft">Account documenting art thefts happening in the NFT space</a></li>
+                <li><a target="_blank" href="https://twitter.com/smdiehl/status/1445795667826208770">https://twitter.com/smdiehl/status/1445795667826208770</a></li>
+              </ul>
             </div>
           </div>
         </div>
