b728e70cd67651c349cab56cd0d03d3bb108c923
Damien Erambert <damien.erambert@me.com>
2021-11-12 14:27:58 -0800

    Setting for confirmation (#717)
    * Add setting for confirmation on mute/block
    
    * Expose setting in the UI

--- src/features/addTweetActions.ts
+++ src/features/addTweetActions.ts
@@ -352,8 +352,11 @@ export const maybeAddTweetActions = makeBTDModule(({settings, TD, jq, mR}) => {
           });
           return;
 
-        case TweetActions.MUTE_ACCOUNT:
-          if (!confirm(`Are you sure you want to mute @${user.screenName}?`)) {
+        case TweetActions.MUTE_ACCOUNT: {
+          const blocked = settings.requireConfirmationForTweetAction
+            ? !confirm(`Are you sure you want to mute @${user.screenName}?`)
+            : false;
+          if (blocked) {
             return;
           }
           jq(document).trigger('uiMuteAction', {
@@ -361,9 +364,13 @@ export const maybeAddTweetActions = makeBTDModule(({settings, TD, jq, mR}) => {
             twitterUser: user,
           });
           return;
+        }
 
-        case TweetActions.BLOCK_ACCOUNT:
-          if (!confirm(`Are you sure you want to block @${user.screenName}?`)) {
+        case TweetActions.BLOCK_ACCOUNT: {
+          const blocked = settings.requireConfirmationForTweetAction
+            ? !confirm(`Are you sure you want to block @${user.screenName}?`)
+            : false;
+          if (blocked) {
             return;
           }
           jq(document).trigger('uiBlockAction', {
@@ -371,6 +378,7 @@ export const maybeAddTweetActions = makeBTDModule(({settings, TD, jq, mR}) => {
             twitterUser: user,
           });
           return;
+        }
       }
     }
   );
