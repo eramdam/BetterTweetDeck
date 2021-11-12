4c3a6d64ff9d901f0bc1085c79eec182246eaf32
Damien Erambert <damien@erambert.me>
2021-11-12 10:07:14 -0500

    Update muteNfts.ts

--- src/features/muteNfts.ts
+++ src/features/muteNfts.ts
@@ -63,7 +63,7 @@ export const muteNftAvatars = makeBTDModule(({TD, jq, settings}) => {
 
     jq(document).on('click', '.nft-avatar-notice-content button', () => {
       jq('.js-modals-container').html(`
-    <div class="js-modal-context overlay overlay-super scroll-v bg-color-overlay anti-nft-modal">
+      <div class="js-modal-context overlay overlay-super scroll-v bg-color-overlay anti-nft-modal">
       <div class="js-modal-inner mdl margin-v--20 s-fluid mdl-lighter-on-dark">
         <div class="js-modal-content modal-content position-rel">
           <div class="padding-b--10 txt-size--16 width--600">
@@ -72,21 +72,22 @@ export const muteNftAvatars = makeBTDModule(({TD, jq, settings}) => {
             </h1>
             <div class="color-twitter-darker-gray txt-size--15 width--430 margin-a-auto">
               <p>
-                This user chose to <a href="https://twitter.com/TheSmarmyBum/status/1443259893411049475" target="_blank">show off their NFT</a> on their profile, so Better TweetDeck hid their tweet.
+                This user chose to <a href="https://twitter.com/TheSmarmyBum/status/1443259893411049475" target="_blank">connect their NFT wallet</a> to their profile, so Better TweetDeck hid their tweet.
                 <br /> <br />
-                As a developer and a human being, <a href="https://twitter.com/Eramdam">I am</a> standing up against NFTs and their dangerous rise in popularity.
+                I am standing up against NFTs and their dangerous rise in popularity.
                 <br /><br />
-                Why is that? As explained in this <a href="https://antsstyle.medium.com/why-nfts-are-bad-the-short-version-48acff22c54b">article</a>, NFTs are bad for two main reasons:
+                Why is that? Because I firmly believe that NFTs are dangerous for a few reasons
               </p>
               <ol>
-                <li>"They are bad for the environment, as they rely on cryptocurrencies that cause huge amounts of carbon emissions."</li>
-                <li>"They are only valuable as tools for money laundering, tax evasion, and greater fool investment fraud."</li>
+                <li>They are bad for the environment, as they rely on cryptocurrencies that cause huge amounts of carbon emissions.</li>
+                <li>They are only valuable as tools for money laundering, tax evasion, and greater fool investment fraud.</li>
+                <li>Their structure facilitates art theft despite being touted as a way to "empower artists"</li>
               </ol>
 
               <p>
-                This is why I am using my platform to raise my voice and try to educate the unaware on the dangers of this technology.
+                This is why I am using my platform to spread awareness about the dangers of this technology.
                 <br /><br />
-                If you think that "actually, NFTs are good", it's fine, you're free to uninstall Better TweetDeck.
+                If you think that "actually, NFTs are good," it's okay. You're free to uninstall Better TweetDeck.
                 <br /><br />
 
                 Otherwise, the two options below are available to you:
@@ -97,10 +98,12 @@ export const muteNftAvatars = makeBTDModule(({TD, jq, settings}) => {
                 <button class="Button Button--link nft-show">Keep showing NFT users</button>
               </div>
 
-              <p>More information available in those articles:</p>
+              <p>Sources:</p>
               <ol>
-                <li><a href="https://antsstyle.medium.com/why-nfts-are-bad-the-short-version-48acff22c54b" target="_blank">Why NFTs are bad: the short version</a></li>
                 <li><a href="https://antsstyle.medium.com/why-nfts-are-bad-the-long-version-2c16dae145e2" target="_blank">Why NFTs are bad: the long version</a></li>
+                <li><a href="https://earth.org/nfts-environmental-impact/">What Are NFTs, And What is Their Environmental Impact?</a></li>
+                <li><a href="https://twitter.com/NFTtheft">Account documenting art thefts happening in the NFT space</a></li>
+                <li><a href="https://twitter.com/smdiehl/status/1445795667826208770">https://twitter.com/smdiehl/status/1445795667826208770</a></li>
               </ol>
 
             </div>
@@ -108,7 +111,7 @@ export const muteNftAvatars = makeBTDModule(({TD, jq, settings}) => {
         </div>
       </div>
     </div>
-    `);
+      `);
     });
   }
 
