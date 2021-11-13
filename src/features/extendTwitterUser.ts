166613b659b0ff6b4901b066b90ef9e66310aaae
Damien Erambert <damien@erambert.me>
2021-11-13 10:02:38 -0500

    Update extendTwitterUser.ts

--- src/features/extendTwitterUser.ts
+++ src/features/extendTwitterUser.ts
@@ -24,15 +24,15 @@ export const extendTwitterUser = makeBTDModule(({TD, jq}) => {
   TD.services.TwitterUser.prototype.OGFromJSON = TD.services.TwitterUser.prototype.fromJSONObject;
 
   TD.services.TwitterUser.prototype.fromJSONObject = function fromJSONObject(blob: any) {
-    var baseTweet = this.OGFromJSON(blob);
+    var baseUser = this.OGFromJSON(blob);
 
-    baseTweet.hasNftAvatar = Boolean(blob.ext_has_nft_avatar);
+    baseUser.hasNftAvatar = Boolean(blob.ext_has_nft_avatar);
     const hasRequiredLabelData =
       blob.ext?.highlightedLabel?.r?.ok?.label &&
       isString(blob.ext?.highlightedLabel?.r?.ok?.label?.badge?.url) &&
       isString(blob.ext?.highlightedLabel?.r?.ok?.label?.description) &&
       isString(blob.ext?.highlightedLabel?.r?.ok?.label?.url.url);
-    baseTweet.highlightedLabel = hasRequiredLabelData
+    baseUser.highlightedLabel = hasRequiredLabelData
       ? {
           badge: blob.ext?.highlightedLabel?.r?.ok?.label?.badge?.url,
           description: blob.ext?.highlightedLabel?.r?.ok?.label?.description,
@@ -40,6 +40,6 @@ export const extendTwitterUser = makeBTDModule(({TD, jq}) => {
         }
       : undefined;
 
-    return baseTweet;
+    return baseUser;
   };
 });
