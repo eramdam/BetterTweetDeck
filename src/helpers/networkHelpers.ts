5467e70c561a7432f18a2bc7d9ab9e4e7b0dfdda
Damien Erambert <damien@erambert.me>
2022-01-07 09:55:36 -0800

    remove un-used network helper

--- src/helpers/networkHelpers.ts
+++ src/helpers/networkHelpers.ts
@@ -7,12 +7,3 @@ export function buildURLWithSearchParams(url: string, searchParams: object) {
 
   return finalUrl.toString();
 }
-
-/** Simple util function to handle status with promises */
-export const statusAndJson = (res: Response) => {
-  if (res.status >= 200 && res.status < 300) {
-    return res.json();
-  }
-
-  return Promise.reject(new Error(res.statusText));
-};
