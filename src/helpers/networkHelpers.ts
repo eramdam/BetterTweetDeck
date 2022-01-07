d253611a9c993a607aaffecd46aa56c4293d9e44
Damien Erambert <damien@erambert.me>
2022-01-07 09:55:48 -0800

    don't re-parse URL when not needed

--- src/helpers/networkHelpers.ts
+++ src/helpers/networkHelpers.ts
@@ -1,5 +1,7 @@
-export function buildURLWithSearchParams(url: string, searchParams: object) {
-  const finalUrl = new URL(url);
+import {isString} from 'lodash';
+
+export function buildURLWithSearchParams(url: URL | string, searchParams: object) {
+  const finalUrl = isString(url) ? new URL(url) : url;
 
   Object.entries(searchParams).forEach(([key, value]) => {
     finalUrl.searchParams.set(key, value);
