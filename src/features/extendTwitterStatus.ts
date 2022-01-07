c3c99f1f05521f8b2fc4f7181a10934fa2460809
Damien Erambert <damien@erambert.me>
2022-01-06 16:41:02 -0800

    Update extendTwitterStatus.ts

--- src/features/extendTwitterStatus.ts
+++ src/features/extendTwitterStatus.ts
@@ -1,20 +1,9 @@
 import {makeBTDModule} from '../types/btdCommonTypes';
-import {TwitterActionEnum, TwitterStatus} from '../types/tweetdeckTypes';
+import {TwitterStatus} from '../types/tweetdeckTypes';
 
 export const extendTwitterStatus = makeBTDModule(({TD, settings}) => {
   TD.services.TwitterAction.prototype.OGFromJSON =
     TD.services.TwitterAction.prototype.fromJSONObject;
-
-  const notificationActionTypes = [
-    TwitterActionEnum.FAVORITE,
-    TwitterActionEnum.FAVORITED_MEDIA,
-    TwitterActionEnum.FAVORITED_MENTION,
-    TwitterActionEnum.FAVORITED_RETWEET,
-    TwitterActionEnum.RETWEETED_MEDIA,
-    TwitterActionEnum.RETWEETED_MENTION,
-    TwitterActionEnum.RETWEETED_RETWEET,
-    TwitterActionEnum.RETWEET,
-  ];
 
   TD.services.TwitterAction.prototype.fromJSONObject = function fromJSONObject(...args: any[]) {
     var baseTweet = this.OGFromJSON(...args) as TwitterStatus;
@@ -32,31 +21,17 @@ export const extendTwitterStatus = makeBTDModule(({TD, settings}) => {
     } else if (baseTweet.targetTweet && baseTweet.targetTweet.card && baseTweet.isAboutYou()) {
       const urlFromCard = baseTweet.targetTweet.card.url;
 
-      if (baseTweet.action && notificationActionTypes.includes(baseTweet.action)) {
-        const isUrlInHtml = baseTweet.targetTweet.htmlText.includes(urlFromCard);
+      const isUrlInHtml = baseTweet.targetTweet.htmlText.includes(urlFromCard);
 
-        if (!isUrlInHtml) {
-          const entityForUrl = baseTweet.targetTweet.entities.urls.find(
-            (u) => u.url === urlFromCard
-          );
-          if (entityForUrl) {
-            baseTweet.targetTweet.htmlText += TD.util.createUrlAnchor({
+      if (!isUrlInHtml) {
+        const entityForUrl = baseTweet.targetTweet.entities.urls.find((u) => u.url === urlFromCard);
+        if (entityForUrl) {
+          baseTweet.targetTweet.htmlText +=
+            ' ' +
+            TD.util.createUrlAnchor({
               ...entityForUrl,
               isUrlForAttachment: false,
             });
-          }
-        }
-      } else {
-        const isUrlInHtml = baseTweet.targetTweet.text.includes(urlFromCard);
-
-        if (!isUrlInHtml) {
-          const entityForUrl = baseTweet.targetTweet.entities.urls.find(
-            (u) => u.url === urlFromCard
-          );
-          if (entityForUrl) {
-            baseTweet.targetTweet.htmlText +=
-              '\n\n' + TD.util.createUrlAnchor({...entityForUrl, isUrlForAttachment: false});
-          }
         }
       }
     }
