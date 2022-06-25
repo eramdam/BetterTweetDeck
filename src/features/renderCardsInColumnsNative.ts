040b1ead4557abcb0b3f303757a792b57c2afd47
Damien Erambert <damien@erambert.me>
2022-06-26 00:32:29 +0200

    Update renderCardsInColumnsNative.ts

--- src/features/renderCardsInColumnsNative.ts
+++ src/features/renderCardsInColumnsNative.ts
@@ -1,42 +1,10 @@
 import {hasProperty, makeIsModuleRaidModule} from '../helpers/typeHelpers';
 import {makeBTDModule} from '../types/btdCommonTypes';
 
-const allowedCardNames = [
-  'amplify',
-  'app',
-  'appplayer',
-  'audio',
-  '745291183405076480:broadcast',
-  'direct_store_link_app',
-  '2586390716:image_direct_message',
-  '745291183405076480:live_event',
-  '2586390716:message_me',
-  '3260518932:moment',
-  '3691233323:periscope_broadcast',
-  'player',
-  'poll2choice_text_only',
-  'poll3choice_text_only',
-  'poll4choice_text_only',
-  'poll2choice_image',
-  'poll3choice_image',
-  'poll4choice_image',
-  'poll2choice_video',
-  'poll3choice_video',
-  'poll4choice_video',
-  'promo_image_app',
-  'promo_image_convo',
-  'promo_video_convo',
-  '2586390716:promo_video_website',
-  'promo_website',
-  'summary',
-  'summary_large_image',
-  '2586390716:video_direct_message',
-];
-
 export const maybeRenderCardsInColumnsNatively = makeBTDModule((options) => {
   const {mR, settings, jq} = options;
 
-  if (!settings.showCardsInsideColumns) {
+  if (settings.showCardsInsideColumns) {
     return;
   }
 
@@ -65,7 +33,7 @@ export const maybeRenderCardsInColumnsNatively = makeBTDModule((options) => {
     return false;
   });
 
-  featureFlagModule.setValueForFeatureFlag('tweetdeck_horizon_web_cards_enabled', allowedCardNames);
+  featureFlagModule.setValueForFeatureFlag('tweetdeck_horizon_web_cards_enabled', []);
   jq(document).on('click', 'article .js-card-container [data-testid="card"]', (ev) => {
     ev.stopPropagation();
   });
